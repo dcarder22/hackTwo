0cd3c2b17a87e7771eda0dd1e2b7c38890680a19
dcarder22 <dcarder22@huskers.unl.edu>
2020-08-31 15:45:51 -0500

    Update airDistance.c
    final code

--- airDistance.c
+++ airDistance.c
@@ -15,8 +15,10 @@ Hack # 2
 int main() {
     const double R = 6371.0;
     
+    //initialize the variables
     double lat1;
     double long1;
+    //get the variables from the user
     printf("Please enter latitude(must be between -90,90) and longitude(must be between -180,180) of the first point:");
     scanf("%lf %lf", &lat1, &long1);
     if ((lat1 > 90) || (lat1 < -90)) {
@@ -26,8 +28,10 @@ int main() {
         printf("longitude not between -180 and 180 ENDING");
     }
     
+    //initialize variables
     double lat2;
     double long2;
+    //get the variables from user
     printf("Please enter the latitude(must be between -90,90) and longitude(must be between -180,180) of the second point:");
     scanf("%lf %lf", &lat2, &long2);
     if ((lat2 > 90) || (lat2 < -90)) {
@@ -37,28 +41,38 @@ int main() {
         printf("longitude not between -180 and 180 ENDING");
     }
     
+    //initialize the variables
     double radianLat1;
     double radianLong1;
     double radianLat2;
     double radianLong2;
     double delta;
+    //calculate the variables
     radianLat1 = lat1 / 180 * M_PI;
     radianLong1 = long1 / 180 * M_PI;
     radianLat2 = lat2 / 180 * M_PI;
     radianLong2 = long2 / 180 * M_PI;
     delta = radianLong2 - radianLong1;
     
+    //initialize the calculate the final distance
     double distance;
     double value;
+    double sLat1, sLat2, cLat1, cLat2, cDelta; 
+    sLat1 = sin(radianLat1);
+    sLat2 = sin(radianLat2);
+    cLat1 = cos(radianLat1);
+    cLat2 = cos(radianLat2);
+    cDelta = cos(delta);
+    value = ((sLat1 * sLat2) + (cLat1 * cLat2 * cDelta)); 
+    distance = acos(value);
+    distance *= R;
     
-    double sinLat1 = sin(radianLat1);
-    double sinlat2 = sin(radianLat2);
-    double cosLat1 = cos(radianLat1);
-    double cosLat2 = cos(radianLat2);
-    double cosDelta = cos(delta);
-    //value = ((sinLat1 * sinLat2) + (cosLat1 * cosLat2 * cosDelta)); 
-    double acos(value);
-    //distance *= R;
+    //format and print the final distance
+    printf("Location Distance\n");
+    printf("=======================\n");
+    printf("Origin:      (%f, %f)\n", lat1, long1);
+    printf("Destination: (%f, %f)\n", lat2, long2);
+    printf("Air distance is %f kms\n", distance);
     
     
     
